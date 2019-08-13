eeaea27fd7c666bcad84370cdd47ae5636adc50a
Nastassia Karatkova <Nastassia_Karatkova@epam.com>
2019-08-13 15:01:09 +0300

    Description of Navigator was done

---
+++ TutorialNavigator/TutorialNavigator.cs
@@ -0,0 +1,32 @@
+﻿using ObjectDescription.TutorialNavigator.Filter;
+using ObjectDescription.TutorialNavigator.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDescription.TutorialNavigator
+{
+    public class TutorialNavigator
+    {
+        public List<TileElement> Tiles
+        {
+            get
+            {
+                //implementation of returning all tiles on the page
+                return new List<TileElement>(); 
+
+            }
+        }
+        public List<CurrentFilter> CurrentFilter
+        {
+            get
+            {
+                //implementation of returning all filters on the page
+                return new List<CurrentFilter>();
+
+            }
+        }
+    }
+}
